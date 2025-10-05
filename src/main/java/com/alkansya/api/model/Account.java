28eaeed1808fcd7306278a61efb4c8d2de288c34
eleriamarkangel <81966579+eleriamarkangel@users.noreply.github.com>
2025-10-05 16:06:51 +0800

    ch10052025-001 - downgrade springboot version
    downgrade springboot version compatible to java 11

--- src/main/java/com/alkansya/api/model/Account.java
+++ src/main/java/com/alkansya/api/model/Account.java
@@ -1,11 +1,7 @@
 package com.alkansya.api.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+//import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.Data;
 
 @Entity
